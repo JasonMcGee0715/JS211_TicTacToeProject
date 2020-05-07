d265919ac1b55fce2b65da0980bc84c41764b6d2
JasonMcGee0715 <jdm0715@gmail.com>
2020-05-07 10:47:16 -0500

    gui working and finished aside from bonus material

--- dom-tictactoe.js
+++ dom-tictactoe.js
@@ -52,6 +52,11 @@ const updateBoard = (id) => {
 
   // @TODO, Your code here: use the above information to change the board variable(array of arrays)
   // HINT: in your browser open up the dev tools -> console
+  board[row][column] = currentMarker
+  if (document.getElementById(id).innerHTML == "") {
+    addMarker(id);
+  }
+
 }
 
 const checkForWin = () => {
@@ -67,19 +72,41 @@ const checkForWin = () => {
 
 const horizontalWin = () => {
   
-if(board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X" || board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O") {
-  return true;
-}
+  if(board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X" || board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O") {
+    return true;
+  };
+  if(board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X" || board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O") {
+    return true;
+  };
+  if(board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X" || board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O") {
+    return true;
+  };
 };
 
 
 const verticalWin = () => {
-  // @TODO, Your code here: to check for vertical wins
-}
+ 
+  if(board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X" || board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O") {
+    return true;
+  };
+  if(board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X" || board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O") {
+    return true;
+  };
+  if(board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X" || board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O") {
+    return true;
+  };
+};
 
 const diagonalWin = () => {
-  // @TODO, Your code here: to check for diagonal wins
+  if(board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X" || board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O") {
+    return true;
+  };
+  if(board[2][0] == "X" && board[1][1] == "X" && board[0][2] == "X" || board[2][0] == "O" && board[1][2] == "O" && board[2][2] == "O") {
+    return true;
+  };
 }
+
+
 
 const changeMarker = () => {
   // ternary operator: if it's an X make it an O, if O make it an X
